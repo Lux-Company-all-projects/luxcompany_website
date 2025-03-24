87fec14e3d533bdfbdaa41cb771d6e3c178f8777
faanrm <faniloniainaa21@gmail.com>
2025-03-24 22:07:54 +0300

    add : download link

--- src/app/features/game/game.component.ts
+++ src/app/features/game/game.component.ts
@@ -23,7 +23,7 @@ export class GameComponent {
       description:
         "Ce jeu s'inspire de l'histoire d'un jeune homme malgache qui fait face à des difficultés suite à l'obtention de son baccalauréat. De plus, il est obligé de trouver un travail pour survivre .Tout au long du jeu, il doit prendre des décisions et assumer les conséquences (chômage,prison,hospitalisation,mort...).Ce jeu a été crée en juillet 2024. Son but est de représenter la vie telle qu'elle est à Madagascar et d'influencer les choix du grand public.",
       image: "/dago_life.png",
-      downloadLink: "#",
+      downloadLink: "https://drive.google.com/file/d/1-3OCOss6ch-GBKu7g--YqjDCvtcnkmgj/view",
     },
   ];
 
