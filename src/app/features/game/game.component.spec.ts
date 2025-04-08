490be55021bf1e8c72c2c6d7dc829a43a28bc14c
faanrm <faniloniainaa21@gmail.com>
2025-04-08 20:59:29 +0300

    prod

--- src/app/features/game/game.component.spec.ts
+++ src/app/features/game/game.component.spec.ts
@@ -1,23 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { GameComponent } from './game.component';
-
-describe('GameComponent', () => {
+import { GameComponent } from "./game.component";
+describe("GameComponent", () => {
   let component: GameComponent;
   let fixture: ComponentFixture<GameComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GameComponent]
-    })
-    .compileComponents();
+      imports: [GameComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GameComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
