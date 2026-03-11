ffbb09693e70bfc9c5b2819f5c2d9340d3606a04
RANDRIA Luca <lucarandrianirina2507@gmail.com>
2026-03-11 11:09:59 +0300

    Change the path of logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,7 +9,7 @@ export const Logo = () => {
       whileTap={{ scale: 0.98 }}
     >
       <img 
-        src="src/assets/lux_company_logo.png" 
+        src="@/src/assets/lux_company_logo.png" 
         alt="Lux Company Logo"
         className="w-8 h-8 sm:w-10 sm:h-10 object-contain"
       />
